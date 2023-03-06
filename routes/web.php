e4dc48bc209777f53a1cc45aab56bf648afd7de7
Gustavo Prado <gustavo.henrique.igt@gmail.com>
2023-03-06 19:37:50 -0300

    Adicionando Rotas
    inserindo Rotas da aplicação.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', )->name('site.index');
+
+Route::get('/home', )->name('site.home');
+
+Route::get('/perfil', )->name('site.perfil');
+
+Route::prefix('/times', function(){
+    Route::get('/meus-times', )->name('times.meus-times');
+    Route::get('/todos-times', )->name('times.todos-times');
 });
+
+Route::get('/campeonatos', )->name('site.campeonatos');
+
+Route::get('/ranking', )->name('site.ranking');
+
+Route::prefix('/configurações', function(){
+    Route::get('/config-perfil',)->name('config.perfil');
+    Route::get('/config-conta',)->name('config.conta');
+    Route::get('/config-localização',)->name('config.localizacao');
+});
+
+Route::get('/chat',)->name('site.chat');
