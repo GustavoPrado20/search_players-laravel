f3ce1a66a3bad2f8a99201da5a176498ebd481e4
Gustavo Prado <gustavo.henrique.igt@gmail.com>
2023-03-06 19:51:00 -0300

    Controllers e Routes
    Criando os Controllers e os adicionando nas rotas

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,25 @@ use App\Http\Controllers;
 |
 */
 
-Route::get('/', )->name('site.index');
+Route::get('/', [Controllers\IndexController::class, ''] )->name('site.index');
 
-Route::get('/home', )->name('site.home');
+Route::get('/home', [Controllers\HomeController::class, ''] )->name('site.home');
 
-Route::get('/perfil', )->name('site.perfil');
+Route::get('/perfil', [Controller\PerfilController::class, ''] )->name('site.perfil');
 
-Route::prefix('/times', function(){
-    Route::get('/meus-times', )->name('times.meus-times');
-    Route::get('/todos-times', )->name('times.todos-times');
+Route::get('/campeonatos', [Controllers\CampeonatosControllers::class, ''] )->name('site.campeonatos');
+
+Route::get('/ranking', [Controllers\RankingController::class, ''] )->name('site.ranking');
+
+Route::get('/chat', [Controllers\ChatController::class, ''] )->name('site.chat');
+
+Route::prefix('/times')->group(function(){
+    Route::get('/meus-times', [Controllers\MeusTimesController::class, ''] )->name('times.meus-times');
+    Route::get('/todos-times', [Controllers\TodosTimesControllers::class, ''] )->name('times.todos-times');
 });
 
-Route::get('/campeonatos', )->name('site.campeonatos');
-
-Route::get('/ranking', )->name('site.ranking');
-
-Route::prefix('/configurações', function(){
-    Route::get('/config-perfil',)->name('config.perfil');
-    Route::get('/config-conta',)->name('config.conta');
-    Route::get('/config-localização',)->name('config.localizacao');
+Route::prefix('/configurações')->group(function(){
+    Route::get('/config-perfil', [Controllers\ConfigPerfilControllers::class, ''])->name('config.perfil');
+    Route::get('/config-conta', [Controllers\ConfigContaController::class, ''] )->name('config.conta');
+    Route::get('/config-localização', [Controllers\ConfigLocalizacao::class, ''])->name('config.localizacao');
 });
-
-Route::get('/chat',)->name('site.chat');
